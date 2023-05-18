a2dbfe0bc75fd6111b010fd67a6563c7dac3f507
chaoliu1995 <chaoliu1995@outlook.com>
2023-05-19 00:31:27 +0800

    完善代码

---
+++ src/main/java/org/example/Config.java
@@ -0,0 +1,86 @@
+package org.example;
+
+public class Config {
+
+    private String privateKeyPath;
+    private String passphrase;
+    private String strictHostKeyChecking;
+    private String serverUser;
+    private String serverHost;
+    private Integer serverPort;
+    private Integer localPort;
+    private String remoteHost;
+    private Integer remotePort;
+
+    public String getPrivateKeyPath() {
+        return privateKeyPath;
+    }
+
+    public void setPrivateKeyPath(String privateKeyPath) {
+        this.privateKeyPath = privateKeyPath;
+    }
+
+    public String getPassphrase() {
+        return passphrase;
+    }
+
+    public void setPassphrase(String passphrase) {
+        this.passphrase = passphrase;
+    }
+
+    public String getStrictHostKeyChecking() {
+        return strictHostKeyChecking;
+    }
+
+    public void setStrictHostKeyChecking(String strictHostKeyChecking) {
+        this.strictHostKeyChecking = strictHostKeyChecking;
+    }
+
+    public String getServerUser() {
+        return serverUser;
+    }
+
+    public void setServerUser(String serverUser) {
+        this.serverUser = serverUser;
+    }
+
+    public String getServerHost() {
+        return serverHost;
+    }
+
+    public void setServerHost(String serverHost) {
+        this.serverHost = serverHost;
+    }
+
+    public Integer getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(Integer serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public Integer getLocalPort() {
+        return localPort;
+    }
+
+    public void setLocalPort(Integer localPort) {
+        this.localPort = localPort;
+    }
+
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public void setRemoteHost(String remoteHost) {
+        this.remoteHost = remoteHost;
+    }
+
+    public Integer getRemotePort() {
+        return remotePort;
+    }
+
+    public void setRemotePort(Integer remotePort) {
+        this.remotePort = remotePort;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,67 @@
 package org.example;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        if(args.length < 1){
+            Print.info("请输入配置文件路径");
+            System.exit(1);
+        }
+        Config config = readConfig(args[0]);
+        JSch jsch = new JSch();
+        //添加私钥
+        jsch.addIdentity(config.getPrivateKeyPath(),config.getPassphrase());
+        Properties sessionConfig = new Properties();
+        //SSH 公钥检查机制 no、ask、yes
+        sessionConfig.put("StrictHostKeyChecking",config.getStrictHostKeyChecking());
+        Session session = null;
+        try{
+            session = jsch.getSession(config.getServerUser(),config.getServerHost(),config.getServerPort());
+            session.setConfig(sessionConfig);
+            session.connect();
+            //本地端口转发
+            //localPort, remoteHost, remotePort
+            session.setPortForwardingL(config.getLocalPort(),config.getRemoteHost(),config.getRemotePort());
+            doShutDownWork(session);
+        }catch (Exception e){
+            e.printStackTrace();
+            if(session != null){
+                session.disconnect();
+            }
+        }
+    }
+
+    private static Config readConfig(String path) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream(path);
+        properties.load(fis);
+        Config config = new Config();
+        config.setStrictHostKeyChecking(properties.getProperty("strictHostKeyChecking"));
+        config.setPrivateKeyPath(properties.getProperty("privateKeyPath"));
+        config.setPassphrase(properties.getProperty("passphrase"));
+        config.setServerUser(properties.getProperty("serverUser"));
+        config.setServerHost(properties.getProperty("serverHost"));
+        config.setServerPort(Integer.valueOf(properties.getProperty("serverPort")));
+        config.setLocalPort(Integer.valueOf(properties.getProperty("localPort")));
+        config.setRemoteHost(properties.getProperty("remoteHost"));
+        config.setRemotePort(Integer.valueOf(properties.getProperty("remotePort")));
+        return config;
+    }
+
+    private static void doShutDownWork(final Session session) {
+        //当前 Java 应用程序相关的运行时对象
+        Runtime run=Runtime.getRuntime();
+        //注册新的虚拟机来关闭钩子
+        run.addShutdownHook(new Thread(() -> {
+            //程序结束时进行的操作
+            Print.info("关闭session");
+            session.disconnect();
+        }));
     }
 }
